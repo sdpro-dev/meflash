76d8d5c203b80f76b3b7799d32de2bde621363dd
Christopher Ciampoli <chris@sdpro.dev>
2021-01-02 14:39:55 -0800

    Latest update added some question, still need more parsing

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,18 @@ import Link from 'next/link'
 import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+import { getSortedDecksData } from '../lib/decks'
 
 export async function getStaticProps () {
-  const allPostsData = getSortedPostsData()
+  const allDecksData = getSortedDecksData()
   return {
     props: {
-      allPostsData
+      allDecksData
     }
   }
 }
 
-export default function Home ({ allPostsData }) {
+export default function Home ({ allDecksData }) {
   return (
     <Layout home>
       <Head>
@@ -30,9 +30,9 @@ export default function Home ({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {allDecksData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
+              <Link href={`/decks/${id}`}>
                 <a>{title}</a>
               </Link>
               <br />
